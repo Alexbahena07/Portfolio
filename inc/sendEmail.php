5a6291ed85fce86d232b25347c2ba631c603aa32
alexbahe <alexbahe@umich.edu>
2023-07-10 23:47:30 -0500

    update is here

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -20,7 +20,7 @@ if($_POST) {
 		$error['email'] = "Please enter a valid email address.";
 	}
 	// Check Message
-	if (strlen($contact_message) < 15) {
+	if (strlen($contact_message) < 5) {
 		$error['message'] = "Please enter your message. It should have at least 15 characters.";
 	}
    // Subject
@@ -29,7 +29,7 @@ if($_POST) {
 
    // Set Message
    $message .= "Email from: " . $name . "<br />";
-	$message .= "Email address: " . $email . "<br />";
+   $message .= "Email address: " . $email . "<br />";
    $message .= "Message: <br />";
    $message .= $contact_message;
    $message .= "<br /> ----- <br /> This email was sent from your site's contact form. <br />";
